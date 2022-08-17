b67301b44037d26835e39a830723594dffac3084
Newton Lomar <newtonlomar16@gmail.com>
2022-08-17 08:54:37 -0300

    creating header component and adding it to App component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <img src='meme-logo' alt='troll face meme' />
+          <h1>Meme Generator</h1>
+        </li>
+        <li>
+          <h3>React Course - Development Knowledge Improvement</h3>
+        </li>
+      </ul>
+    </header>
+  );
+}
