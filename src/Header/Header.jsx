f8996cb3e0edf3afc5511fb1ff2c07eb350083de
Newton Lomar <newtonlomar16@gmail.com>
2022-08-24 08:15:41 -0300

    finished header component

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,15 +3,13 @@ import logo from '../assets/troll_face.png';
 export default function Header() {
   return (
     <header className='header'>
-      <ul className='header__ul'>
-        <li>
-          <img className='logo' src={logo} alt='troll face meme' />
-          <h1>Meme Generator</h1>
-        </li>
-        <li>
-          <h3>React Course - Development Knowledge Improvement</h3>
-        </li>
-      </ul>
+      <div className='header__logo-h1'>
+        <img className='logo' src={logo} alt='troll face meme' />
+        <h1 className='h1'>Meme Generator</h1>
+      </div>
+      <div>
+        <h3 className='h3'>React Course - Development Knowledge Improvement</h3>
+      </div>
     </header>
   );
 }
