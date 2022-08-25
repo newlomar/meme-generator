42e8efa1d67944149b2a89ede725ebd1d8b90e8b
Newton Lomar <newtonlomar16@gmail.com>
2022-08-25 09:23:43 -0300

    adding meme form

---
+++ src/Meme/Meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme() {
+  return (
+    <form>
+      <section>
+        <input type='text' />
+        <input type='text' />
+      </section>
+      <button type='submit'></button>
+    </form>
+  );
+}
