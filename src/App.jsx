672744093fd937f4c13b4f54437dded2b0ab4069
Newton Lomar <newtonlomar16@gmail.com>
2022-08-26 09:27:15 -0300

    adding meme component to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header/Header';
+import Meme from './Meme/Meme';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <Meme />
     </div>
   );
 }
