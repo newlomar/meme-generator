2d253725b43fe8013a51499522da61da75bc7b49
Newton Lomar <newtonlomar16@gmail.com>
2022-08-27 15:52:48 -0300

    styling meme component

--- src/Meme/Meme.jsx
+++ src/Meme/Meme.jsx
@@ -1,11 +1,15 @@
 export default function Meme() {
   return (
-    <form>
-      <section>
-        <input type='text' />
-        <input type='text' />
-      </section>
-      <button type='submit'>Get new meme image</button>
-    </form>
+    <main className='main'>
+      <form>
+        <section className='main__inputs'>
+          <input className='input' type='text' placeholder='Top text' />
+          <input className='input' type='text' placeholder='Bottom text' />
+        </section>
+        <button className='button' type='submit'>
+          Get new meme image
+        </button>
+      </form>
+    </main>
   );
 }
